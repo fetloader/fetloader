3b75cefd0a82c4ea339321fc5f16ae70b6808019
Max <cuwuless@gmail.com>
2021-08-05 11:58:04 +0400

    Update COMPILE.bat

--- COMPILE.bat
+++ COMPILE.bat
@@ -1,1 +1,1 @@
-.\Ahk2Exe\Ahk2Exe.exe /in loader.ahk /out loader.exe /bin ".\Ahk2Exe\ANSI 32-bit.bin" 
+.\Ahk2Exe\Ahk2Exe.exe /ahk "C:\Program Files\AutoHotkey\AutoHotkey.exe" /in loader.ahk /out loader.exe /bin ".\Ahk2Exe\ANSI 32-bit.bin" 
